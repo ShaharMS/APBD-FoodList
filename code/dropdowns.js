40ea285b9c4af0614a4d65a1a3c455abe36ebc4e
Shahar Marcus <shaharmsecond@gmail.com>
2024-07-10 01:21:07 +0300

    database page

--- code/dropdowns.js
+++ code/dropdowns.js
@@ -4,14 +4,36 @@
 var translationMatrix = [];
 var languageIndex = 0;
 function activateDropdowns() {
+    let about = document.getElementById("about");
+    let aboutBounds = about.getBoundingClientRect();
+
     let contribute = document.getElementById("contribute");
     let contributeBounds = contribute.getBoundingClientRect();
     
     let contact = document.getElementById("contact");
     let contactBounds = contact.getBoundingClientRect();
 
+    let aboutMenu = document.getElementById("about-menu");
     let contributeMenu = document.getElementById("contribute-menu");
     let contactMenu = document.getElementById("contact-menu");
+
+
+    const showAboutMenu = (e) => {console.log("show about");
+        aboutMenu.style.display = "block";
+    }
+
+    const hideAboutMenu = (e) => {console.log("hide about");
+        aboutMenu.style.display = "none";
+    }
+
+    const switchAboutMenu = (e) => {console.log("switch about");
+        hideContributeMenu();
+        if (aboutMenu.style.display == "block") {
+            hideAboutMenu();
+        } else {
+            showAboutMenu();
+        }
+    }
 
     const showContributeMenu = (e) => {console.log("show trib");
         contributeMenu.style.display = "block";
@@ -47,18 +69,27 @@ function activateDropdowns() {
         }
     }
 
+
+    about.addEventListener("mouseenter", showAboutMenu)
+    aboutMenu.addEventListener("mouseenter", showAboutMenu)
+    aboutMenu.addEventListener("mouseleave", hideAboutMenu)
+    about.addEventListener("mouseleave", hideAboutMenu)
+
     contribute.addEventListener("mouseenter", showContributeMenu)
     contributeMenu.addEventListener("mouseenter", showContributeMenu)
     contributeMenu.addEventListener("mouseleave", hideContributeMenu)
     contribute.addEventListener("mouseleave", hideContributeMenu)
 
-    contribute.addEventListener("touchstart", switchContributeMenu, {passive: true})
-    contact.addEventListener("touchstart", switchContactMenu, {passive: true})
-
     contact.addEventListener("mouseenter", showContactMenu)
     contactMenu.addEventListener("mouseenter", showContactMenu)
     contactMenu.addEventListener("mouseleave", hideContactMenu)
     contact.addEventListener("mouseleave", hideContactMenu)
+
+
+    about.addEventListener("touchstart", switchAboutMenu, {passive: true})
+    contribute.addEventListener("touchstart", switchContributeMenu, {passive: true})
+    contact.addEventListener("touchstart", switchContactMenu, {passive: true})
+
 
     // Discord button
     let discord = document.getElementById("discord");
