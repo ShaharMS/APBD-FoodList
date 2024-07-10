c21a4493d3423592e9313ebd5571341c4d7fa7b9
Shahar Marcus <shaharmsecond@gmail.com>
2024-07-10 21:19:46 +0300

    meats. Attempt to resolve name sorting

--- code/table-loader.js
+++ code/table-loader.js
@@ -1,5 +1,8 @@
 const parameters = new URLSearchParams(window.location.search);
 
+/**
+ * @type {HTMLTableElement}
+ */
 let table = document.getElementById("food-list");
 
 const response = await fetch(window.location.origin + "/content/table.html")
@@ -10,14 +13,55 @@ table.innerHTML = html
  */
 let rows = Array.from(table.getElementsByTagName("tr"));
 
+const TABLE = document.getElementById("food-list")
+TABLE.innerHTML = rows.map(row => row.outerHTML).join("\n")
+console.log("tables done, translating if needed");
+
+activateSwitches()
+activateSearchbar()
+activateDropdowns()
+if (!parameters.has("lang") || (parameters.has("lang") && parameters.get("lang") === "en")) {
+    await translate("en");
+} else await translate(parameters.get("lang"));
+
+// translate resets HTML references
+
+table = document.getElementById("food-list");
+rows = Array.from(table.getElementsByTagName("tr"));
+
+// Post-process th table
+
 // Sort if needed
 if (parameters.has("sort")) {
+
+    /**
+     * Sorts the rows of a table based on a provided predicate function.
+     *
+     * @param {HTMLTableElement} table - The table to sort.
+     * @param {(a: HTMLTableRowElement, b: HTMLTableRowElement) => number} predicate - The function used to determine the sort order.
+     * @return {void} This function does not return a value.
+     */
+    function sortTable(table, predicate) {
+        var rs = Array.from(table.rows).slice(1); // Convert rows collection to array, excluding header row
+        rs.sort(predicate); // Sort rows array using the provided predicate function
+
+        // Clear the existing table body
+        while (table.rows.length > 1) {
+            table.deleteRow(1);
+        }
+
+        // Re-append sorted rows to the table
+        rs.forEach(function (row) {
+            table.tBodies[0].appendChild(row);
+        });
+    }
+
     console.log("Sorting by " + parameters.get("sort"));
     let header = rows.shift();
     let sort = parameters.get("sort");
     switch (sort) {
         case "name-d": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aName = a.getElementsByTagName("td")[0].innerText;
                 let bName = b.getElementsByTagName("td")[0].innerText;
                 if (aName > bName) return 1;
@@ -27,7 +71,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "name-u": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aName = a.getElementsByTagName("td")[0].innerText;
                 let bName = b.getElementsByTagName("td")[0].innerText;
                 if (aName < bName) return 1;
@@ -37,7 +81,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "glycemic-index-d": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aIndex = parseFloat(a.getElementsByTagName("td")[2].getElementsByTagName("span")[0].innerText);
                 let bIndex = parseFloat(b.getElementsByTagName("td")[2].getElementsByTagName("span")[0].innerText);
                 if (aIndex == -1 && bIndex == -1) return 0;
@@ -50,7 +94,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "glycemic-index-u": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aIndex = parseFloat(a.getElementsByTagName("td")[2].getElementsByTagName("span")[0].innerText);
                 let bIndex = parseFloat(b.getElementsByTagName("td")[2].getElementsByTagName("span")[0].innerText);
                 if (aIndex == -1 && bIndex == -1) return 0;
@@ -63,7 +107,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "glycemic-load-d": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aLoad = parseFloat(a.getElementsByTagName("td")[3].getElementsByTagName("span")[0].innerText);
                 let bLoad = parseFloat(b.getElementsByTagName("td")[3].getElementsByTagName("span")[0].innerText);
                 if (aLoad < 0 && bLoad < 0) return 0;
@@ -76,7 +120,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "glycemic-load-u": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aLoad = parseFloat(a.getElementsByTagName("td")[3].getElementsByTagName("span")[0].innerText);
                 let bLoad = parseFloat(b.getElementsByTagName("td")[3].getElementsByTagName("span")[0].innerText);
                 if (aLoad < 0 && bLoad < 0) return 0;
@@ -89,7 +133,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "sugars-d": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aSugars = parseFloat(a.getElementsByTagName("td")[4].getElementsByTagName("span")[0].innerText);
                 let bSugars = parseFloat(b.getElementsByTagName("td")[4].getElementsByTagName("span")[0].innerText);
                 if (aSugars < 0 && bSugars < 0) return 0;
@@ -102,7 +146,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "sugars-u": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aSugars = parseFloat(a.getElementsByTagName("td")[4].getElementsByTagName("span")[0].innerText);
                 let bSugars = parseFloat(b.getElementsByTagName("td")[4].getElementsByTagName("span")[0].innerText);
                 if (aSugars < 0 && bSugars < 0) return 0;
@@ -115,12 +159,12 @@ if (parameters.has("sort")) {
             break;
         }
         case "carbs-d": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aCarbs = parseFloat(a.getElementsByTagName("td")[5].getElementsByTagName("span")[0].innerText);
                 let bCarbs = parseFloat(b.getElementsByTagName("td")[5].getElementsByTagName("span")[0].innerText);
                 if (aCarbs < 0 && bCarbs < 0) return 0;
                 if (aCarbs < 0) return 1;
-                if (bCarbs < 0) return -1;              
+                if (bCarbs < 0) return -1;
                 if (aCarbs > bCarbs) return 1;
                 if (aCarbs < bCarbs) return -1;
                 return 0;
@@ -128,7 +172,7 @@ if (parameters.has("sort")) {
             break;
         }
         case "carbs-u": {
-            rows.sort((a, b) => {
+            sortTable(table, (a, b) => {
                 let aCarbs = parseFloat(a.getElementsByTagName("td")[5].getElementsByTagName("span")[0].innerText);
                 let bCarbs = parseFloat(b.getElementsByTagName("td")[5].getElementsByTagName("span")[0].innerText);
                 if (aCarbs < 0 && bCarbs < 0) return 0;
@@ -147,26 +191,7 @@ if (parameters.has("sort")) {
 
 console.log("postprocessing table");
 
-
-// All references are lost after postprocessing, except for the rows array
-
-const TABLE = document.getElementById("food-list")
-TABLE.innerHTML = rows.map(row => row.outerHTML).join("\n")
-console.log("tables done, translating if needed");
-
-activateSwitches()
-activateSearchbar()
-activateDropdowns()
-if (!parameters.has("lang") || (parameters.has("lang") && parameters.get("lang") === "en")) {
-    await translate("en");
-} else await translate(parameters.get("lang"));
-
-// translate resets HTML references
-
-table = document.getElementById("food-list");
-rows = table.getElementsByTagName("tr");
-
-// Post-process th table
+// Fix floating point precision errors
 
 for (let i = 0; i < rows.length; i++) {
     let cells = rows[i].getElementsByTagName("td");
@@ -176,7 +201,7 @@ for (let i = 0; i < rows.length; i++) {
             let gl = cells[3].getElementsByTagName("span")[0].getElementsByTagName("span")[0];
             let sugar = cells[4].getElementsByTagName("span")[0];
             let carbs = cells[5].getElementsByTagName("span")[0];
-            
+
             if (gl.innerText.includes(".")) {
                 gl.innerText = gl.innerText.split(".")[0] + "." + gl.innerText.split(".")[1].substring(0, 3);
                 if (gl.innerText.endsWith(".000")) gl.innerText = gl.innerText.substring(0, gl.innerText.length - 4);
@@ -187,7 +212,7 @@ for (let i = 0; i < rows.length; i++) {
                 sugar.innerText = sugar.innerText.split(".")[0] + "." + sugar.innerText.split(".")[1].substring(0, 3);
                 if (sugar.innerText.endsWith(".000")) sugar.innerText = sugar.innerText.substring(0, sugar.innerText.length - 4);
                 else if (sugar.innerText.endsWith("00")) sugar.innerText = sugar.innerText.substring(0, sugar.innerText.length - 2);
-                else if (sugar.innerText.endsWith("0")) sugar.innerText = sugar.innerText.substring(0, sugar.innerText.length - 1); 
+                else if (sugar.innerText.endsWith("0")) sugar.innerText = sugar.innerText.substring(0, sugar.innerText.length - 1);
             }
             if (carbs.innerText.includes(".")) {
                 carbs.innerText = carbs.innerText.split(".")[0] + "." + carbs.innerText.split(".")[1].substring(0, 3);
@@ -197,7 +222,7 @@ for (let i = 0; i < rows.length; i++) {
             }
         }
         fixFloatingPoint();
-        
+
         let input = cells[0].getElementsByTagName("input")[0];
         input.addEventListener("change", fixFloatingPoint);
 
@@ -221,6 +246,11 @@ for (let i = 0; i < rows.length; i++) {
                 par.innerText = "~"
                 par.style.marginInlineStart = "0";
                 element.after(par)
+            } else if (element.hasAttribute("less-than")) {
+                let par = document.createElement("span");
+                par.innerText = "<"
+                par.style.marginInlineStart = "0";
+                element.before(par)
             }
         }
     }
